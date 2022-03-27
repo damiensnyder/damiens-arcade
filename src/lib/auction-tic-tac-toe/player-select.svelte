d63b5bf3d8cb1873a980f3c4d82e98645d9221d0
Damien Snyder <signup@damiensnyder.com>
2022-03-27 11:51:31 -0700

    add start button

--- src/lib/auction-tic-tac-toe/player-select.svelte
+++ src/lib/auction-tic-tac-toe/player-select.svelte
@@ -2,25 +2,49 @@
   import type { ActionCallback } from "$lib/types";
   import { Side } from "$lib/auction-tic-tac-toe/types";
   import type { AuctionTTTViewpoint } from "$lib/auction-tic-tac-toe/types";
-  import X from "$lib/auction-tic-tac-toe/x.svelte";
-  import O from "$lib/auction-tic-tac-toe/o.svelte";
-import PlayerJoiner from "./player-joiner.svelte";
+  import PlayerJoiner from "./player-joiner.svelte";
 
   export let gamestate: AuctionTTTViewpoint;
   export let callback: ActionCallback;
+
+  function startGame() {
+    callback({
+      type: "startGame"
+    });
+  }
 </script>
 
-<div class="player-select">
-  <PlayerJoiner gamestate={gamestate} callback={callback} side={Side.X} />
-  <PlayerJoiner gamestate={gamestate} callback={callback} side={Side.O} />
+<div class="outer">
+  <div class="inner">
+    <PlayerJoiner gamestate={gamestate} callback={callback} side={Side.X} />
+    <PlayerJoiner gamestate={gamestate} callback={callback} side={Side.O} />
+  </div>
+  {#if gamestate.isHost && gamestate.players.length === 2}
+    <button class="big-button" on:submit={startGame}>
+      START
+    </button>
+  {:else if gamestate.isHost}
+    <button class="big-button" disabled>
+      START
+    </button>
+  {/if}
 </div>
 
 <style>
-  .player-select {
-    justify-content: space-evenly;
+  .outer {
     align-self: stretch;
     padding-left: 2rem;
     margin-left: 2.5rem;
     border-left: 2px solid var(--text-1);
+  }
+
+  .inner {
+    flex: 1;
+    justify-content: space-evenly;
+  }
+
+  button {
+    justify-self: flex-end;
+    margin-bottom: 0;
   }
 </style>
