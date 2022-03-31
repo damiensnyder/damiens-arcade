eaf66c2b8f02a02c1aa8530eb00e0a0ccbc6ecaa
Damien Snyder <signup@damiensnyder.com>
2022-03-31 14:35:26 -0700

    fix crashing error on backend

--- src/lib/auction-tic-tac-toe/utils.ts
+++ src/lib/auction-tic-tac-toe/utils.ts
@@ -1,6 +1,5 @@
 import { Side } from "$lib/auction-tic-tac-toe/types";
 import type { Player } from "$lib/auction-tic-tac-toe/types";
-import type { Viewer } from "$lib/types";
 
 export function oppositeSideOf(side: Side): Side {
   return side === Side.X ? Side.O : Side.X;
@@ -9,21 +8,21 @@ export function oppositeSideOf(side: Side): Side {
 export function winningSide(squares: Side[][]): Side {
   for (const side of [Side.X, Side.O]) {
     for (let i = 0; i < 3; i++) {
-      if ((this.squares[i][0] === side &&
-            this.squares[i][1] === side &&
-            this.squares[i][2] === side) ||
-          (this.squares[0][i] === side &&
-            this.squares[1][i] === side &&
-            this.squares[2][i] === side)) {
+      if ((squares[i][0] === side &&
+            squares[i][1] === side &&
+            squares[i][2] === side) ||
+          (squares[0][i] === side &&
+            squares[1][i] === side &&
+            squares[2][i] === side)) {
         return side;
       }
     }
-    if ((this.squares[0][0] === side &&
-          this.squares[1][1] === side &&
-          this.squares[2][2] === side) ||
-        (this.squares[2][0] === side &&
-          this.squares[1][1] === side &&
-          this.squares[0][2] === side)) {
+    if ((squares[0][0] === side &&
+          squares[1][1] === side &&
+          squares[2][2] === side) ||
+        (squares[2][0] === side &&
+          squares[1][1] === side &&
+          squares[0][2] === side)) {
       return side;
     }
   }
