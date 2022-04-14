473acbbe4c996025ef3799bbe6a081a78cf35045
Damien Snyder <signup@damiensnyder.com>
2022-04-13 19:57:32 -0700

    add more instructions

--- src/lib/auction-tic-tac-toe/instruction.svelte
+++ src/lib/auction-tic-tac-toe/instruction.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { players, turnPart, whoseTurnToNominate } from "$lib/auction-tic-tac-toe/stores";
+  import { players, turnPart, whoseTurnToBid, whoseTurnToNominate } from "$lib/auction-tic-tac-toe/stores";
   import { TurnPart } from "$lib/auction-tic-tac-toe/types";
   import { pov } from "$lib/stores";
 </script>
@@ -7,6 +7,12 @@
 <p>
   {#if $turnPart === TurnPart.Nominating && $players[$whoseTurnToNominate].controller === $pov}
     Click a square to put it up for auction.
+  {:else if $turnPart === TurnPart.Nominating}
+    Waiting for {$whoseTurnToNominate} to nominate a square.
+  {:else if $turnPart === TurnPart.Bidding && $players[$whoseTurnToBid].controller === $pov}
+    Decide how much you want to bid on the square, or if you want to pass.
+  {:else if $turnPart === TurnPart.Bidding}
+    Waiting for {$whoseTurnToNominate} to bid.
   {:else}
     &nbsp;
   {/if}
