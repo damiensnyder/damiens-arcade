3d3ebedf379e4d4c891a14bf372607859abbf771
Damien Snyder <signup@damiensnyder.com>
2022-06-21 22:21:12 -0700

    add star ratings

--- src/lib/tourney/fighter-info.svelte
+++ src/lib/tourney/fighter-info.svelte
@@ -2,12 +2,14 @@
   import { lastAction } from "$lib/stores";
   import type { FighterStats } from "$lib/tourney/types";
   import { StatName } from "$lib/tourney/types";
+  import StarRating from "./star-rating.svelte";
 
   export let name: string;
   export let stats: FighterStats;
   export let index: number;
   export let imgUrl: string;
-  // export let description: string;
+  export let description: string;
+  export let flavor: string;
   // export let yearsLeft: number;
 
   function pick() {
@@ -24,13 +26,17 @@
 </div>
 <div class="horiz">
   <img src={imgUrl} width="150" height="150" alt={name} />
-  <div class="horiz">
-    <div>
+  <div class="horiz info">
+    <div class="stats">
       {#each Object.entries(StatName) as statEntry}
         <div class="horiz stat-name">
-          {statEntry[0]}: {stats[statEntry[1]]}
+          {statEntry[0]}&nbsp;<StarRating rating={stats[statEntry[1]]} />
         </div>
       {/each}
+    </div>
+    <div class="description">
+      <p>{description}</p>
+      <p>{flavor}</p>
     </div>
   </div>
 </div>
@@ -53,9 +59,34 @@
     margin: 0.15rem 0 0 1rem;
   }
 
+  .stats {
+    align-items: stretch;
+    margin: 0 0.5rem 0.5rem 0;
+  }
+
   .stat-name {
     flex: 1;
     justify-content: space-between;
     text-transform: capitalize;
+  }
+
+  .info {
+    align-items: stretch;
+  }
+
+  .description {
+    flex: 1;
+    justify-content: start;
+    align-items: flex-start;
+    margin: 0 0 0.5rem 0.5rem;
+  }
+
+  p {
+    margin: 0 0 0.25rem 0;
+  }
+
+  p:last-child {
+    margin: 0;
+    font-style: italic;
   }
 </style>
---
+++ src/lib/tourney/star-rating.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let rating: number;
+</script>
+
+<div class="horiz">
+  {#each [1, 3, 5, 7, 9] as num}
+    {#if rating > num}
+      <svg width="20px" height="20px" viewBox="0 0 10 10">
+        <path fill="#ec6" d="M5 7.457 L8.09 9.51 L7.092 5.937 L10 3.632 L6.293 3.477 L5 0 L3.707 3.477 L0 3.632 L2.908 5.937 L1.91 9.51 L5 7.457 Z"/>
+      </svg>
+    {:else if rating === num}
+      <svg width="20px" height="20px" viewBox="0 0 10 10">
+        <defs>
+          <linearGradient id="grad">
+            <stop offset="50%" stop-color="#ec6"/>
+            <stop offset="50%" stop-color="#888"/>
+          </linearGradient>
+        </defs>
+        <path fill="url(#grad)" d="M5 7.457 L8.09 9.51 L7.092 5.937 L10 3.632 L6.293 3.477 L5 0 L3.707 3.477 L0 3.632 L2.908 5.937 L1.91 9.51 L5 7.457 Z"/>
+      </svg>
+    {:else}
+      <svg width="20px" height="20px" viewBox="0 0 10 10">
+        <path fill="#888" d="M5 7.457 L8.09 9.51 L7.092 5.937 L10 3.632 L6.293 3.477 L5 0 L3.707 3.477 L0 3.632 L2.908 5.937 L1.91 9.51 L5 7.457 Z"/>
+      </svg>
+    {/if}
+  {/each}
+</div>
