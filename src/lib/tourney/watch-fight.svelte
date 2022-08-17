c0492df5455c9a967bade543abe4e807db212211
Damien Snyder <signup@damiensnyder.com>
2022-08-17 13:51:27 -0700

    fix major bug in fight viewer
    i don't like that i had to make the whole tick display at once for this to happen, but it's preferable to completely not working

--- src/lib/tourney/watch-fight.svelte
+++ src/lib/tourney/watch-fight.svelte
@@ -9,9 +9,8 @@
   let flipped: AnimationState[] = [];
   let tick: number = 0;
   let tickInterval = null;
-  let eventInterval = null;
   let lastEvent: string = "";
-  let slowdown: number = 1;
+  let tickLength: number = 200;
 
   enum AnimationState {
     Stationary = "0deg",
@@ -24,38 +23,44 @@
   function enterEvents(): void {
     try {
       eventLog = JSON.parse("[" + eventLogRaw.replaceAll("][", "],[") + "]");
+      tick = 0;
     } catch (e) {
       window.alert("you done messed up");
     }
   }
-
+  
   function play(): void {
-    tickInterval = setInterval(() => {
-      if (tick < eventLog.length) {
-        let i = 0;
-        eventInterval = setInterval(() => {
-          if (i < eventLog[tick].length) {
-            handleEvent(eventLog[tick][i], i);
-          }
-          i++;
-          if (i === eventLog[tick].length) {
-            clearInterval(eventInterval);
-          }
-        }, 200 / eventLog[tick].length * slowdown);
-        tick++;
-      } else {
-        clearInterval(tickInterval);
-      }
-    }, 200 * slowdown);
+    tickInterval = setInterval(step, tickLength);
   }
-
+  
   function pause(): void {
     clearInterval(tickInterval);
   }
-
+  
+  function step(): void {
+    if (tick < eventLog.length) {
+      eventLog[tick].forEach(handleEvent);
+      /* const processNextEvent = (i) => {
+        handleEvent(eventLog[tick][i]);
+        if (i + 1 < eventLog[tick].length) {
+          setTimeout(() => processNextEvent(i + 1), tickLength / eventLog[tick].length);
+        } else {
+          console.debug(i + 1);
+          console.debug(eventLog[tick]);
+        }
+      };
+      if (eventLog[tick].length > 0) {
+        processNextEvent(0);
+      } */
+      
+      tick++;
+    } else {
+      clearInterval(tickInterval);
+    }
+  }
+  
   function restart(): void {
     clearInterval(tickInterval);
-    clearInterval(eventInterval);
     fighters = [];
     rotation = [];
     flipped = [];
@@ -63,13 +68,14 @@
     play();
   }
 
-  function handleEvent(event: MidFightEvent, _i: number): void {
+  function handleEvent(event: MidFightEvent): void {
+    console.debug("Event in tick " + tick + ":");
+    console.debug(event);
     if (event.type === "spawn") {
-      if (tick > 1) {
-        window.alert(lastEvent);
-      }
       fighters.push(event.fighter);
+      fighters = fighters;
       rotation.push(AnimationState.Stationary);
+      rotation = rotation;
     } else if (event.type === "move") {
       fighters[event.fighter].x = event.x;
       fighters[event.fighter].y = event.y;
@@ -84,13 +90,16 @@
   <div class="controls">
     <button on:click={play} on:submit={play}>Play</button>
     <button on:click={pause} on:submit={pause}>Pause</button>
+    <button on:click={step} on:submit={step}>Step</button>
     <button on:click={restart} on:submit={restart}>Restart</button>
     <button on:click={enterEvents} on:submit={enterEvents}>Enter events</button>
     <textarea rows={1} bind:value={eventLogRaw}></textarea>
-    <label>Slowdown
-      <input type="range" min="1" max="50" bind:value={slowdown} />
+    <label>Tick length
+      <input type="range" min="100" max="5000" bind:value={tickLength} />
     </label>
-    <p>{lastEvent}</p>
+    <p>Tick: {@debug tick}</p>
+    <p>Fighters: {@debug fighters}</p>
+    <p>Last event: {lastEvent}</p>
   </div>
   <div class="arena">
     {#each fighters as f, i}
