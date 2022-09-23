e189e8e58216093ec1d4add4ad81506d7c6bfdf4
Damien Snyder <signup@damiensnyder.com>
2022-09-23 00:46:36 -0700

    display the bracket on the tournament page

---
+++ src/lib/tourney/bracket.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { Bracket } from "$lib/tourney/types";
+  import { teams } from "$lib/tourney/stores";
+
+  export let left: Bracket | null = null;
+  export let right: Bracket | null = null;
+  export let winner: number;
+</script>
+
+<div class="horiz">
+  <div>
+    {#if left !== null}
+      <svelte:self {...left} />
+    {/if}
+    {#if right !== null}
+      <svelte:self {...right} />
+    {/if}
+  </div>
+  {#if winner !== null}
+    <p class="winner">{$teams[winner].name}</p>
+  {:else}
+    <p class="winner tbd">winner</p>
+  {/if}
+</div>
+
+<style>
+  .winner {
+    width: 6rem;
+    padding: 0.5rem;
+    text-align: right;
+    border-right: 2px solid var(--bg-fun-1);
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .tbd {
+    color: var(--text-fun-2);
+    font-style: italic;
+  }
+</style>
---
+++ src/lib/tourney/pick-fighters.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Bracket from "$lib/tourney/bracket.svelte";
+  import { bracket } from "$lib/tourney/stores";
+</script>
+
+<div>
+  <h2>Bracket</h2>
+  <Bracket {...$bracket} />
+</div>
