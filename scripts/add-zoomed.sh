1ddfa86bf5ea2142a092efa4d59849d2ce23da0c
Damien Snyder <signup@damiensnyder.com>
2023-01-29 23:33:15 -0800

    add helper scripts

--- scripts/add-zoomed.sh
+++ scripts/add-zoomed.sh
@@ -1,8 +1,10 @@
+# trim whitespace and save to zoomed/equipment
 mogrify -trim +repage -background none -path static/zoomed/equipment static/equipment/*.png
 
+# make the aspect ratio square
 for filename in static/zoomed/equipment/*.png; do
-  ww=`convert $filename -ping -format "%w" info:`
-  hh=`convert $filename -ping -format "%h" info:`
-  greater=`[[ ${ww} -gt ${hh} ]] && echo $ww || echo $hh`
+  width=`convert $filename -ping -format "%w" info:`
+  width=`convert $filename -ping -format "%h" info:`
+  greater=`[[ ${widt} -gt ${height} ]] && echo $width || echo $height`
   mogrify -gravity center -background none -extent ${greater}x${greater} $filename;
 done
---
+++ scripts/set-node-version.sh
@@ -0,0 +1,2 @@
+# i only use this locally. in prod this is unnecessary
+export PATH=/usr/local/bin/$PATH
