effd673a94d489417e14fff1c4265a5a4784545d
Damien Snyder <53230081+damiensnyder@users.noreply.github.com>
2023-12-11 23:21:12 -0800

    make drag n drop work

--- src/routes/daily-qless/+page.svelte
+++ src/routes/daily-qless/+page.svelte
@@ -1,11 +1,43 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import "../../styles/global.css";
   import "../../styles/techno.css";
 
-  let grid: string[][] = [...Array(13)].map((_, i) => Array(12).fill(""));
-  grid[5].splice(4, 4, "a", "b", "b", "c");
-  grid[6].splice(4, 4, "d", "f", "h", "n");
-  grid[7].splice(4, 4, "o", "o", "p", "w");
+  let grid: string[][] = [...Array(11)].map((_, i) => Array(12).fill(""));
+  grid[0].splice(4, 4, "a", "b", "b", "c");
+  grid[1].splice(4, 4, "d", "f", "h", "n");
+  grid[2].splice(4, 4, "o", "o", "p", "w");
+  let shownGrid: string[][] = grid.map(row => row.slice());
+
+  let dragging: { x: number, y: number } = { x: -1, y: -1 };
+  let over: { x: number, y: number } = { x: -1, y: -1 };
+
+  function handleDragStart(e: DragEvent): void {
+    dragging = this;
+  }
+
+  function handleDragOver(e: DragEvent): void {
+    if (dragging.x !== -1 && shownGrid[this.x][this.y] === "") {
+      const prevOver = over;
+      over = this;
+      shownGrid[dragging.x][dragging.y] = "";
+      shownGrid[prevOver.x][prevOver.y] = "";
+      shownGrid[this.x][this.y] = grid[dragging.x][dragging.y];
+    }
+  }
+
+  function handleDragEnd(e: DragEvent): void {
+    grid[over.x][over.y] = grid[dragging.x][dragging.y];
+    grid[dragging.x][dragging.y] = "";
+    dragging = { x: -1, y: -1 };
+    over = { x: -1, y: -1 };
+    shownGrid = grid.map(row => row.slice());
+    checkForWin();
+  }
+  
+  function checkForWin(): void {
+
+  }
 </script>
   
 <svelte:head>
@@ -15,10 +47,17 @@
 <h1>Daily Q-less</h1>
 
 <div class="grid-outer">
-  <div class="grid-inner">
-    {#each grid as row, x}
+  <div class="grid-inner"
+      on:dragover|preventDefault={() => {}}>
+    {#each shownGrid as row, x}
       {#each row as cell, y}
-        <div class={grid[x][y] === "" ? "cell" : "cell filled"} style:grid-area={`${x + 1} / ${y + 1}`}>
+        <div class={cell === "" ? "cell" : "cell filled"}
+            style:grid-area={`${x + 1} / ${y + 1}`}
+            draggable={grid[x][y] !== ""}
+            on:dragstart={handleDragStart.bind({ x, y })}
+            on:dragenter={handleDragOver.bind({ x, y })}
+            on:dragend={handleDragEnd}
+            on:dragover|preventDefault={() => {}}>
           {cell}
         </div>
       {/each}
@@ -37,16 +76,15 @@
     overflow-x: scroll;
     overflow-y: scroll;
     border-radius: 0.75rem;
-    background-color: var(--bg-3);
+    background-color: var(--bg-1);
   }
 
   .grid-inner {
     display: grid;
     gap: 15px;
-    grid-template-rows: repeat(13, 60px);
+    grid-template-rows: repeat(11, 60px);
     grid-template-columns: repeat(12, 60px);
-    /* margin: auto; */
-    padding: 10px;
+    padding: 12px;
     font-size: 30px;
     font-weight: 700;
     cursor: grab;
