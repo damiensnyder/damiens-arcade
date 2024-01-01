cc7bc7eb925a55bc8cff0a216addafa430933b44
Damien Snyder <53230081+damiensnyder@users.noreply.github.com>
2023-12-31 23:30:18 -0800

    update ratings

--- analysis/compare_lists.py
+++ analysis/compare_lists.py
@@ -2,41 +2,38 @@ import json
 import csv
 
 
-l1 = {}
-
-with open("analysis/wordlist1.txt") as f:
-    for line in f.readlines():
-        if "\t" in line:
-            word = line.split("\t")[0].lower()
-            if 3 <= len(word) <= 12:
-                l1[word] = 5
+definitions = {}
+realness = {}
 
 with open("analysis/ratings.csv") as f:
     reader = csv.reader(f, delimiter=",", quotechar="\"")
     for row in reader:
-        l1[row[0]] = int(row[1])
+        realness[row[0]] = int(row[1])
 
-l2 = set()
+with open("analysis/wordlist1.txt") as f:
+    for line in f.readlines():
+        if "\t" in line:
+            [word, definition] = line.split("\t", 1)
+            word = word.lower()
+            definition = definition.strip()
+            if 3 <= len(word) <= 12:
+                if word not in realness:
+                    realness[word] = 5
 
 with open("analysis/wordlist2.txt") as f:
     for line in f.readlines():
         if " " in line:
-            word = line.split(" ")[0].lower()
+            [word, definition] = line.split(" ", 1)
+            word = word.lower()
+            definition = definition.strip()
+            definitions[word] = definition
             if 3 <= len(word) <= 12:
-                l2.add(word)
+                if word not in realness:
+                    realness[word] = 5
 
-l2_only = sorted(list([w for w in l2 if (w not in l1)]))
-
-l1_only = sorted(list([w for w in l1 if (w not in l2) and (l1[w] == 5)]))
-
-print(len(l2))
-print(len(l1))
-print(len(l2_only))
-print(len(l1_only))
-
-print(l2_only[:30])
-print(l1_only[:30])
-
-with open("analysis/ratings.csv", "a") as f:
-    for w in l2_only + l1_only:
-        f.write(w + ",2\n")
+print(realness["born"])
+print(definitions["born"])
+print(realness["clonk"])
+print(definitions["clonk"])
+print(realness["methodical"])
+print(definitions["methodical"])
--- analysis/qless_roll_finder.py
+++ analysis/qless_roll_finder.py
@@ -3,21 +3,34 @@ import csv
 import random
 
 
+definitions = {}
+realness = {}
 
 with open("analysis/ratings.csv") as f:
     reader = csv.reader(f, delimiter=",", quotechar="\"")
-    realness = {}
     for row in reader:
         realness[row[0]] = int(row[1])
 
-with open("analysis/definitions.csv") as f:
-    reader = csv.reader(f, delimiter=",", quotechar="\"")
-    definitions = {}
-    for row in reader:
-        if len(row[0]) > 2:
-            definitions[row[0].lower()] = row[1]
-            if row[0].lower() not in realness:
-                realness[row[0].lower()] = 5
+with open("analysis/wordlist1.txt") as f:
+    for line in f.readlines():
+        if "\t" in line:
+            [word, definition] = line.split("\t", 1)
+            word = word.lower()
+            definition = definition.strip()
+            if 3 <= len(word) <= 12:
+                if word not in realness:
+                    realness[word] = 5
+
+with open("analysis/wordlist2.txt") as f:
+    for line in f.readlines():
+        if " " in line:
+            [word, definition] = line.split(" ", 1)
+            word = word.lower()
+            definition = definition.strip()
+            definitions[word] = definition
+            if 3 <= len(word) <= 12:
+                if word not in realness:
+                    realness[word] = 5
 
 with open("analysis/grids.json") as f:
     good = json.load(f)
