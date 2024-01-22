4ad5e45f91334a0fc55abf899bb4f1c9b7b6f093
Damien Snyder <signup@damiensnyder.com>
2024-01-21 21:45:28 -0800

    add tooltips on fighter stats

--- src/lib/mayhem-manager/fighter-info.svelte
+++ src/lib/mayhem-manager/fighter-info.svelte
@@ -13,13 +13,20 @@
   $: isTurnToPick = index > -1 &&
       ($gameStage === "preseason" || $draftOrder[$spotInDraftOrder] === $ownTeamIndex);
   
-  // const NO_STATS: FighterStats = {
-  //   strength: -1,
-  //   accuracy: -1,
-  //   energy: -1,
-  //   speed: -1,
-  //   toughness: -1
-  // }
+  function tooltip(stat: StatName, value: number): string {
+    value = Math.round(value);
+    if (stat === StatName.Strength) {
+      return `Deals ${0.5 + 0.1 * value}x base damage with melee weapons`;
+    } else if (stat === StatName.Accuracy) {
+      return `Hits ${25 + 5 * value}% of ranged attacks`;
+    } else if (stat === StatName.Energy) {
+      return `Takes ${9 - 0.6 * value} seconds to charge`;
+    } else if (stat === StatName.Speed) {
+      return `Moves ${2.5 + 0.5 * value} m/s; ${2 * value}% dodge chance on melee attacks`;
+    } else {
+      return `Incoming damage is multiplied by ${1.25 - 0.05 * value}`;
+    }
+  }
 
   function pick(): void {
     if ($gameStage === "preseason") {
@@ -52,8 +59,8 @@
   <div class="horiz info">
     <div class="stats">
       {#each Object.entries(StatName) as statEntry}
-        <div class="horiz stat-name">
-          {statEntry[0]}&nbsp;<StarRating rating={fighter.stats[statEntry[1]]} oldRating={(fighter.oldStats ?? fighter.stats)[statEntry[1]]} />
+        <div class="horiz stat-name" title={tooltip(statEntry[1], fighter.stats[statEntry[1]])}>
+          {statEntry[1]}&nbsp;<StarRating rating={fighter.stats[statEntry[1]]} oldRating={(fighter.oldStats ?? fighter.stats)[statEntry[1]]} />
         </div>
       {/each}
     </div>
